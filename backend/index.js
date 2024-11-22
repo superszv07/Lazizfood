01569166296c2911ea907bf285f1b4dd4cb2dd46
superszv07 <shahbazahmad2001@gmail.com>
2024-11-22 21:16:36 +0530

    6 commit

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
-
+require('dotenv').config()
+const path= require('path')
 const express = require('express')
 const app = express()
 const port = 5000
@@ -6,13 +7,16 @@ const port = 5000
 const mongoDB =require("./db")
 mongoDB();
 
-const path = "F:\react\mernapp\backend";
+const PORT = process.env.PORT || port
+const dirname = path.resolve();
+
 
 app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type,Accept");
   next();
 })
+
 
 app.use(express.json())//add express json otherwise it will not work
 app.use('/api', require("./Routes/CreateUser"));//to hit createUser end point
@@ -21,6 +25,8 @@ app.use('/api', require("./Routes/OrderData"));
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use(express.static(path.join(dirname, "/frontend/build")));
+app.get('*',(req,res)=>{res.sendFile(path.resolve(dirname,"frontend" ,"build","index.html" ));})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
